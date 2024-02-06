4d25ced3b33b0b28d728f9b0e9b44a1532ba8a6f
rohan samanta <129646057+shadowrohan@users.noreply.github.com>
2024-02-06 18:01:14 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -11,9 +11,12 @@ $("#nums").change(function(){
 $(".enter_btn").click(function(){
     if(checkDisplay()){
         randomAns();
-        setTimeout(() => {
-            converter();
-        }, 300);
+    }
+});
+
+display.keypress(function(e){
+    if((e.key=='Enter')&&(checkDisplay())){
+        randomAns();
     }
 });
 
@@ -73,13 +76,32 @@ function checkDisplay(){
     }
 }
 
+function errorMsg(){
+    var msg;
+    if(num == 'bin'){
+        msg = 'binary';
+    }
+    if(num == 'oct'){
+        msg = 'octal';
+    }
+    if(num == 'dec'){
+        msg = 'decimal';
+    }
+    if(num == 'hex'){
+        msg = 'hexa-decimal';
+    }
+    return 'This is not a ' + msg + ' number!';
+}
+
 let toastBox = document.getElementById('toastBox');
 function showError(){
+    const errorLogo = '<svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" class="warning_svg" width="24"><path d="M480-280q17 0 28.5-11.5T520-320q0-17-11.5-28.5T480-360q-17 0-28.5 11.5T440-320q0 17 11.5 28.5T480-280Zm-40-160h80v-240h-80v240Zm40 360q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z"/></svg>';
+    const cencelBtn = '<svg class="cencel_btn" xmlns="http://www.w3.org/2000/svg" height="30" viewBox="0 -960 960 960" width="30"><path d="m336-280-56-56 144-144-144-143 56-56 144 144 143-144 56 56-144 143 144 144-56 56-143-144-144 144Z"/></svg>';
     $(".enter_btn").prop('disabled', true);
     display.css("background","rgba(253, 193, 193, 0.8)");
     let toast = document.createElement('div');
     toast.classList.add('toast');
-    toast.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" class="warning_svg" width="24"><path d="M480-280q17 0 28.5-11.5T520-320q0-17-11.5-28.5T480-360q-17 0-28.5 11.5T440-320q0 17 11.5 28.5T480-280Zm-40-160h80v-240h-80v240Zm40 360q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z"/></svg>  suapokar er bacha <svg class="cencel_btn" xmlns="http://www.w3.org/2000/svg" height="30" viewBox="0 -960 960 960" width="30"><path d="m336-280-56-56 144-144-144-143 56-56 144 144 143-144 56 56-144 143 144 144-56 56-143-144-144 144Z"/></svg>';
+    toast.innerHTML = errorLogo + errorMsg() + cencelBtn;
     toast.style.color = "red";
     toastBox.appendChild(toast);
     $(".toast").hide().fadeIn();
@@ -135,9 +157,11 @@ function converter(){
 
 function randomAns() {
     var count=0;
-    setInterval(function(){
+    var interval = setInterval(() => {
+        console.log("hit");
         if(count > 5){
-            clearInterval();
+            clearInterval(interval);
+            converter();
             return;
         }
         var randomNumber = Math.floor(Math.random()*99999999);
